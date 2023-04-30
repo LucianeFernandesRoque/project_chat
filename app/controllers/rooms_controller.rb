089e278047186fc822a0aa1cbb46c0c28aaaa699
Luciane Fernandes Roque <lucianefernandescdf@gmail.com>
2023-04-30 20:40:54 -0300

    [Feature: create UserRoom relationship]

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -20,6 +20,7 @@ class RoomsController < ApplicationController
 
     respond_to do |format|
       if @room.save
+        UserRoom.create(room: @room, user: current_user)
         format.turbo_stream { render turbo_stream: turbo_stream.append('rooms', partial: 'shared/room', locals: {room: @room }) }
       else
         format.turbo_stream { render turbo_stream: turbo_stream.replace('room_form', partial: 'rooms/form', locals: { room: @room }) }
