9453761492c4d6a02d4c8a494ad6f80b99467bb7
Luciane Fernandes Roque <lucianefernandescdf@gmail.com>
2023-05-07 15:46:22 -0300

    [Feat]: Create a web-socket

--- app/models/user_room.rb
+++ app/models/user_room.rb
@@ -1,4 +1,13 @@
 class UserRoom < ApplicationRecord
   belongs_to :user
   belongs_to :room
+
+  after_commit on: :create do
+    broadcast_append_to(
+      'user_rooms_channel',
+      partial: 'shared/room',
+      locals: { room: Room.find(room_id) },
+      target: "user_#{user_id}_rooms"
+    )
+  end
 end
