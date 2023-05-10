0b0c71c5f1737253bcdad39ede6575bdf24099d9
Luciane Fernandes Roque <lucianefernandescdf@gmail.com>
2023-05-10 19:18:09 -0300

    [Feat]: create view form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   post 'add/user', to: 'rooms#add_user'
-  resources :rooms
+  resources :rooms do
+    resources :messages
+  end
   devise_for :users
   root 'rooms#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
