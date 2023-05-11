6585f2fb3c55c363029d9753d7d3ee19192fb9aa
Luciane Fernandes Roque <lucianefernandescdf@gmail.com>
2023-05-11 18:10:28 -0300

    [Feat]: create messages with turbo stream

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,12 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :room
+
+  after_create_commit :broadcast_room_messages_create
+
+  private
+
+  def broadcast_room_messages_create
+    broadcast_append_to('room_messages_channel', partial: 'messages/message', locals: { message: self }, target: "room_messages_div")
+  end
 end
